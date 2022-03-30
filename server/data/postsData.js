5e20ae4d72b8a1add71e52848bc2e180fa8fa17b
nogueirasena <nogueirasena@hotmail.com>
2022-03-30 12:58:06 -0300

    teste implementado

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -3,3 +3,23 @@ const database = require('../infra/database');
 exports.getPosts = function () {
 	return database.query('select * from post');
 };
+
+exports.getPost = function (id) {
+	return database.oneOrNone('select * from post where id = $1', [id]);
+};
+
+exports.getPostByTitle = function (title) {
+	return database.oneOrNone('select * from post where title = $1', [title]);
+};
+
+exports.savePost = function (post) {
+	return database.one('insert into post (title, content) values ($1, $2) returning *', [post.title, post.content]);
+};
+
+exports.updatePost = function (id, post) {
+	return database.none('update post set title = $1, content = $2 where id = $3', [post.title, post.content, id]);
+};
+
+exports.deletePost = function (id) {
+	return database.none('delete from post where id = $1', [id]);
+};
